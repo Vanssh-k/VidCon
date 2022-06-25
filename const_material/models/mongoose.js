90b3bef312d07aa9e08cdf233802843035bfd7c8
Vansh <vanshkapoor2001@gmail.com>
2022-06-26 00:09:51 +0530

    fixed untracked files

--- const_material/models/mongoose.js
+++ const_material/models/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectUrl = "mongodb://127.0.0.1:27017/video_con";
-
+// const connectUrl = "mongodb://127.0.0.1:27017/video_con";
+const connectUrl =
+  "mongodb+srv://Admin-vansh:<password>@cluster0.jb0li.mongodb.net/?retryWrites=true&w=majority";
 try {
   mongoose.connect(connectUrl);
   console.log("mongoose connected!");
