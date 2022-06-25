8b86c00996cebefb89e52fd9ec6993b5c05fc484
Vansh <vanshkapoor2001@gmail.com>
2022-06-26 00:52:14 +0530

    fixed untracked files

--- public/script.js
+++ public/script.js
@@ -23,13 +23,13 @@ navigator.mediaDevices
   })
   .then((stream) => {
     addVideoStream(my_video, stream);
-    myPeer.on('call', call => {
-      call.answer(stream)
-      const video = document.createElement('video')
-      call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-      })
-    })
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
 
     socket.on("user-joined", (userId) => {
       connectToNewUser(userId, stream);
